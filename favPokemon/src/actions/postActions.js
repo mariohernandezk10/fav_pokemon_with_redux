7742b0d946b66a7e9fe71c97a850009f92b58cec
Mario Hernandez <mariohernandezk10@gmail.com>
2021-08-24 15:38:40 -0500

    deleted unnecessary code

--- favPokemon/src/actions/postActions.js
+++ favPokemon/src/actions/postActions.js
@@ -3,7 +3,7 @@ import { FETCH_POSTS } from './types';
 export const fetchPosts = () => dispatch => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=251')
         .then(res => res.json())
-        .then(posts => console.log(posts))
+        .then(posts => console.log(posts.results))
         // .then(posts => 
         //     dispatch({
         //     type: FETCH_POSTS,
