b80c94d7b4f5004b78c48c7911a55ce598c57219
Mario Hernandez <mariohernandezk10@gmail.com>
2021-08-24 15:32:22 -0500

    comment dispatch

--- redux/src/actions/postActions.js
+++ redux/src/actions/postActions.js
@@ -4,12 +4,12 @@ export const fetchPosts = () => dispatch => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=251')
         .then(res => res.json())
         .then(posts => console.log(posts))
-        .then(posts => 
-            dispatch({
-            type: FETCH_POSTS,
-            payload: posts
-            })
-        )
+        // .then(posts => 
+        //     dispatch({
+        //     type: FETCH_POSTS,
+        //     payload: posts
+        //     })
+        // )
 
 }
 // SIDE NOTE: TOO MANY ".then()" INSIDE THE FETCH WILL GIVE AN ERROR
